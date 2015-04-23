6ff27a4f2203fb7644c565abb78f66ad12bf57b1
Anthony Perritano <aperritano@gmail.com>
2015-04-23 02:02:31 -0400

    videozone commented

--- src/main/java/ltg/evl/uic/poster/widgets/VideoZone.java
+++ src/main/java/ltg/evl/uic/poster/widgets/VideoZone.java
@@ -1,56 +1,50 @@
 package ltg.evl.uic.poster.widgets;
 
-import com.google.common.io.Resources;
-import processing.core.PApplet;
-import processing.video.Movie;
-import vialab.SMT.Touch;
 import vialab.SMT.Zone;
-
-import java.io.File;
 
 /**
  * Created by aperritano on 8/1/14.
  */
 public class VideoZone extends Zone {
-    private Movie movie;
+    // private Movie movie;
 
     public VideoZone(String name, int x, int y, int width, int height) {
         super(name, x, y, width, height);
-        setup();
+        // setup();
     }
 
-    public void setup() {
-
-        //URL url = this.getClass().getResource("transit.mov");
-        File f = new File(Resources.getResource("transit.mov").getPath());
-        String newPath = f.getPath().replaceAll("%20", " ");
-        movie = new Movie(this.applet, newPath);
-    }
-
-    @Override
-    public void draw() {
-        background(200);
-        image(movie, 0, 0, 175, 175);
-    }
-
-    @Override
-    public void pickDraw() {
-        rect(175, 0, 25, 175);
-    }
-
-    @Override
-    public void press(Touch touch) {
-        super.press(touch);
-        rect(0, 0, 170, 175);
-        PApplet.println("PRESSED");
-    }
-
-
-    public Movie getMovie() {
-        return movie;
-    }
-
-    public void movieEvent(Movie m) {
-        m.read();
-    }
+//    public void setup() {
+//
+//        //URL url = this.getClass().getResource("transit.mov");
+//        File f = new File(Resources.getResource("transit.mov").getPath());
+//        String newPath = f.getPath().replaceAll("%20", " ");
+//        movie = new Movie(this.applet, newPath);
+//    }
+//
+//    @Override
+//    public void draw() {
+//        background(200);
+//        image(movie, 0, 0, 175, 175);
+//    }
+//
+//    @Override
+//    public void pickDraw() {
+//        rect(175, 0, 25, 175);
+//    }
+//
+//    @Override
+//    public void press(Touch touch) {
+//        super.press(touch);
+//        rect(0, 0, 170, 175);
+//        PApplet.println("PRESSED");
+//    }
+//
+//
+//    public Movie getMovie() {
+//        return movie;
+//    }
+//
+//    public void movieEvent(Movie m) {
+//        m.read();
+//    }
 }
